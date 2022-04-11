dc1294be848701848e4957942c7e80d2000a79c5
micro@san <skdeep98150@gmail.com>
2022-04-11 16:48:37 -0700

    created note taking area function in app.js

--- src/app.js
+++ src/app.js
@@ -5,3 +5,19 @@ const notes = [
     id: 1 
   }
 ]
+const plusButton = document.querySelector('.fa-circle-plus')
+const noteCreate = `<textarea class='note-textarea' rows="25" cols="90" placeholder="Write your notes here, first line will be the title"></textarea><div class ="buttons-fornote"><button class='save-button'>Save</button><button class='cancel-button'>Cancel</button></div>`
+const writeNote = document.querySelector(".write-note-area")
+
+
+//Add text area
+function addNewText() {
+  writeNote.insertAdjacentHTML("afterbegin", noteCreate);
+  const cancelNoteBtn = document.querySelector(".cancel-button"); //selects the cancel button
+  const saveTextBtn = document.querySelector(".save-button"); //selects the save button
+  saveTextBtn.addEventListener("click", savePad);
+  cancelNoteBtn.addEventListener("click", cancelNote); //listen the cancel 
+  plusButton.removeEventListener("click", addNewText);
+  plusButton.remove() //removes the plusButton
+}
+plusButton.addEventListener("click", addNewText);
