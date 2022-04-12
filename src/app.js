6f312977a4c4e67acb8d3d2befe27a265f866d1a
micro@san <skdeep98150@gmail.com>
2022-04-11 17:25:34 -0700

    create function to read the side nav and close it after read

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ const noteCreate = `<textarea class='note-textarea' rows="25" cols="90" placehol
 const writeNote = document.querySelector(".write-note-area")
 const noteDisplayArea = document.querySelector(".notes-list")
 const createNote = document.querySelector('.create-note-area')
+const readNote = document.querySelector(".read-note-area")
 
 //Add text area
 function addNewText() {
@@ -43,3 +44,23 @@ function savePad() {
   newNotepad();
   cancelNote();
 }
+
+//Open nav bar text
+function displayText(event) {
+  if (event.target.localName === "li") {
+    content = event.target.innerText;}
+  for (const note of notes) {
+    if (note.title == content) body_note = note.noteBody;}
+var readNoteList= `<div class="text-list"><p>Note:${body_note}</p></div><div class = "close-button"><button class="closeBtn">Close</button></div>`
+readNote.insertAdjacentHTML("afterbegin", readNoteList);
+
+//Close the nav bar text
+  const closeBtn = document.querySelector(".closeBtn");
+  function closeRead() {
+    while (readNote.hasChildNodes()) {
+      readNote.removeChild(readNote.firstChild);
+    }
+  }
+  closeBtn.addEventListener("click", closeRead);
+}
+noteDisplayArea.addEventListener("click", displayText);
