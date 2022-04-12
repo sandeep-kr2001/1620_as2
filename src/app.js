e4d50886d531d32a00db45d00638b7f1e54306a6
micro@san <skdeep98150@gmail.com>
2022-04-11 17:05:27 -0700

    created functionality for cancel button and save button

--- src/app.js
+++ src/app.js
@@ -8,7 +8,8 @@ const notes = [
 const plusButton = document.querySelector('.fa-circle-plus')
 const noteCreate = `<textarea class='note-textarea' rows="25" cols="90" placeholder="Write your notes here, first line will be the title"></textarea><div class ="buttons-fornote"><button class='save-button'>Save</button><button class='cancel-button'>Cancel</button></div>`
 const writeNote = document.querySelector(".write-note-area")
-
+const noteDisplayArea = document.querySelector(".notes-list")
+const createNote = document.querySelector('.create-note-area')
 
 //Add text area
 function addNewText() {
@@ -21,3 +22,24 @@ function addNewText() {
   plusButton.remove() //removes the plusButton
 }
 plusButton.addEventListener("click", addNewText);
+
+//Cancel text area
+function cancelNote() {
+  while (writeNote.hasChildNodes()) {
+    writeNote.removeChild(writeNote.firstChild);
+  }
+  createNote.appendChild(plusButton)
+  plusButton.addEventListener("click", addNewText);
+}
+//Save text area 
+function newNotepad() {
+  const notePad = document.querySelector(".note-textarea");
+  const titles = notePad.value.split("\n")[0]; //split the text so first line becomes title 
+  notes.push({ title: titles, noteBody: notePad.value, id: notes.length +1 }); //add 1 to next id and save the values 
+  const listNote = `  <li>${titles}</li>`; //create a new list in ul
+  noteDisplayArea.insertAdjacentHTML("beforeend", listNote);
+}
+function savePad() {
+  newNotepad();
+  cancelNote();
+}
